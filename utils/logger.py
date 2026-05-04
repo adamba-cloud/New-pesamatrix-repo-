f1c8595f066ab84841c4b3a9ffe2a939acd5febc
adamba-cloud <philipcraig364@gmail.com>
2026-05-04 16:38:12 +0300

    Add logger setup function in logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,8 @@
+import logging
 
+def setup_logger():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    return logging.getLogger("copy-trading")
